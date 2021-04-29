74921b2d34ce2eb3470bd2cdb1af85b3cdac25cd
Xelopat <xelopat@gmail.com>
2021-04-29 16:13:33 +0300

    design+

--- main.py
+++ main.py
@@ -1,28 +1,40 @@
+import os
 import sys
 from urllib import request
+from PyQt5 import QtWidgets
+from PyQt5.uic import loadUiType
 
-from PyQt5 import QtWidgets, uic
+from my_vk import vk_class
+
+main_win, _ = loadUiType(os.path.join('res', 'design', 'main.ui'))
+properties_win, _ = loadUiType(os.path.join('res', 'design', 'properties.ui'))
 
 
-class my_window(QtWidgets.QMainWindow):
+class MainWindow(QtWidgets.QMainWindow, main_win):
     def __init__(self):
-        super(my_window, self).__init__()
-        self.ui = Properties()
-        self.ui.setupUi(self)
-
-    def main(self):
-        self.ui = Main()
-        self.ui.setupUi(self)
-
-    def load_img(self, link):
-        resource = request.urlopen(link)
-        out = open("img.jpg", 'wb')
-        out.write(resource.read())
-        out.close()
+        QtWidgets.QMainWindow.__init__(self)
+        self.setupUi(self)
+        self.properties.clicked.connect(lambda: w.setCurrentIndex(1))
+        self.commit.clicked.connect(lambda: w.setCurrentIndex(1))
 
 
-app = QtWidgets.QApplication([])
-application = my_window()
-application.show()
+class Properties(QtWidgets.QMainWindow, properties_win):
+    def __init__(self):
+        QtWidgets.QMainWindow.__init__(self)
+        self.setupUi(self)
+        self.cancle.clicked.connect(lambda: w.setCurrentIndex(0))
 
-sys.exit(app.exec())
+
+if __name__ == "__main__":
+    vk = vk_class()
+    app = QtWidgets.QApplication(sys.argv)
+    # Append windows
+    main_window = MainWindow()
+    properties_window = Properties()
+    # Set window
+    w = QtWidgets.QStackedWidget()
+    w.addWidget(main_window)
+    w.addWidget(properties_window)
+    w.resize(800, 600)
+    w.show()
+    sys.exit(app.exec_())
